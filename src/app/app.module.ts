c414fcb519aa874ca151e68c2ec4ec69fc9fe826
Javier <jmartin@grupoloyal.com>
2022-12-12 14:22:13 +0100

    Modularidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyCoreModule } from 'src/lib/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonServicesModule } from './common-services';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, MainModule, CommonServicesModule, SecurityModule, MyCoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
